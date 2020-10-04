13ceb5090b60e5731265763b8667e0178c66b263
Zachary Perkins <ezmac028@gmail.com>
2020-10-04 19:16:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   {
-    animal = "cat";
+    var animal = "cat";
   }
   return animal
 }
