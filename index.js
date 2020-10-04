b79fffe724dcc2f432df844d23e8ae31fc0af8b6
Zachary Perkins <ezmac028@gmail.com>
2020-10-04 19:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  animal = animal;
+  animal = cat;
   return animal
 }
 
